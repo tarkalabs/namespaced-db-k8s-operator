7cc79754475c17d3485a1d043988fb7e00906c5e
Madhava Reddy SV <madhav@tarkalabs.com>
2022-03-03 16:18:36 +0530

    refactored code

--- controllers/namespaceddbcreds_controller.go
+++ controllers/namespaceddbcreds_controller.go
@@ -60,25 +60,14 @@ type NamespacedDBCredsReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the NamespacedDBCreds object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
 func (r *NamespacedDBCredsReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
-
 	log.Info(fmt.Sprintf("Reconciling started at %s", time.Now().Format("2006-01-02 15:04:05")))
 
 	// Fetch the NamespacedDBCreds instance
 	instance := &dbv1alpha1.NamespacedDBCreds{}
-	err := r.Get(ctx, req.NamespacedName, instance)
-
-	if err != nil {
+	if err := r.Get(ctx, req.NamespacedName, instance); err != nil {
 		if errors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request.
 			log.Info("NamespacedDBCreds object not found. Ignoring since object must be deleted.")
 			return ctrl.Result{}, nil
 		}
@@ -87,103 +76,14 @@ func (r *NamespacedDBCredsReconciler) Reconcile(ctx context.Context, req ctrl.Re
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	// Check if the instance is marked to be deleted, which is
-	// indicated by the deletion timestamp being set.
-	if instance.GetDeletionTimestamp() != nil {
-		// Check and run if any finalizers are present on the object
-		if controllerutil.ContainsFinalizer(instance, crdFinalizer) {
-			// Run finalizer logic for this CRD
-			if err := r.finalizeNamespacedDbCredDelete(instance, log); err != nil {
-				return ctrl.Result{}, err
-			}
-			// Remove finalizer if work is done
-			controllerutil.RemoveFinalizer(instance, crdFinalizer)
-			err := r.Update(ctx, instance)
-			if err != nil {
-				return ctrl.Result{}, err
-			}
-		}
-		return ctrl.Result{}, nil
+	if _, err := checkAndUpdateFinalizers(*r, ctx, instance, log); err != nil {
+		log.Error(err, "Unable to update finalizers")
+		return ctrl.Result{}, err
 	}
 
-	// Add finalizer for this CR if not exists
-	if !controllerutil.ContainsFinalizer(instance, crdFinalizer) {
-		controllerutil.AddFinalizer(instance, crdFinalizer)
-		err = r.Update(ctx, instance)
-		if err != nil {
-			return ctrl.Result{}, err
-		}
-	}
-
-	db, err := sql.Open("mysql", getDataSourceName(instance))
-	if err != nil {
-		log.Error(err, "Unable to connect to database of "+instance.Name)
+	if _, err := checkAndUpdateSecrets(*r, ctx, instance, log); err != nil {
+		log.Error(err, "Unable to update secrets in all namespaces")
 		return ctrl.Result{RequeueAfter: time.Minute}, err
-	}
-	defer db.Close()
-
-	// Fetch the Namespaces
-	namespaces := &corev1.NamespaceList{}
-	err = r.List(ctx, namespaces)
-	if err != nil {
-		log.Error(err, "Unable to fetch Namespaces")
-		return ctrl.Result{RequeueAfter: time.Minute}, err
-	} else {
-		// Create db secret for each Namespace
-		for _, namespace := range namespaces.Items {
-			existingSecret := &corev1.Secret{}
-
-			err = r.Get(ctx, client.ObjectKey{Name: instance.Name, Namespace: namespace.Name}, existingSecret)
-
-			if err != nil && errors.IsNotFound(err) {
-				dbPassword := generateNewPassword(16)
-				if !createDatabase(db, namespace.Name, dbPassword) {
-					return ctrl.Result{RequeueAfter: time.Minute}, err
-				}
-
-				secret := getSecret(instance, &namespace, nil, dbPassword)
-				err = r.Create(ctx, &secret)
-				if err != nil {
-					log.Error(err, "Unable to create Secret in Namespace: "+namespace.Name)
-					return ctrl.Result{RequeueAfter: time.Minute}, err
-				} else {
-					// Should set controller & owner references but our usecase is to create secret in all namespaces
-					// If we set references, we will get an error saying `cross-namespace references are disallowed`.
-					log.Info("Secret created in Namespace: " + namespace.Name)
-				}
-			} else if err != nil {
-				log.Error(err, "Unable to fetch Namespace secret: "+namespace.Name)
-				return ctrl.Result{RequeueAfter: time.Minute}, err
-			} else {
-				dbPort, _ := strconv.Atoi(string(existingSecret.Data["port"]))
-				if (instance.Spec.DBHost != string(existingSecret.Data["host"])) || (instance.Spec.DBPort != dbPort) {
-					secret := getSecret(instance, &namespace, existingSecret, "")
-
-					if !createDatabase(db, namespace.Name, generateNewPassword(16)) {
-						log.Error(err, "Unable to create database for Namespace: "+namespace.Name)
-						return ctrl.Result{RequeueAfter: time.Minute}, err
-					}
-
-					err = r.Update(ctx, &secret)
-					if err != nil {
-						log.Error(err, "Unable to update Secret in Namespace: "+namespace.Name)
-						return ctrl.Result{RequeueAfter: time.Minute}, err
-					} else {
-						log.Info("Secret updated in Namespace: " + namespace.Name)
-					}
-				} else {
-					if !checkDatabaseAndUser(db, namespace.Name) {
-						log.Error(nil, "Database or User got deleted for Namespace: "+namespace.Name)
-						if !createDatabase(db, namespace.Name, string(existingSecret.Data["password"])) {
-							log.Error(err, "Unable to create database for Namespace: "+namespace.Name)
-							return ctrl.Result{RequeueAfter: time.Minute}, err
-						} else {
-							log.Info("Database & User are created for Namespace: " + namespace.Name)
-						}
-					}
-				}
-			}
-		}
 	}
 
 	log.Info(fmt.Sprintf("Reconciling ended at %s", time.Now().Format("2006-01-02 15:04:05")))
@@ -221,6 +121,110 @@ func (r *NamespacedDBCredsReconciler) finalizeNamespacedDbCredDelete(instance *d
 	return nil
 }
 
+func checkAndUpdateFinalizers(r NamespacedDBCredsReconciler, ctx context.Context, instance *dbv1alpha1.NamespacedDBCreds, log logr.Logger) (ctrl.Result, error) {
+	// Check if the instance is marked to be deleted, which is
+	// indicated by the deletion timestamp being set.
+	if instance.GetDeletionTimestamp() != nil {
+		// Check and run if any finalizers are present on the object
+		if controllerutil.ContainsFinalizer(instance, crdFinalizer) {
+			// Run finalizer logic for this CRD
+			if err := r.finalizeNamespacedDbCredDelete(instance, log); err != nil {
+				return ctrl.Result{}, err
+			}
+			// Remove finalizer if work is done
+			controllerutil.RemoveFinalizer(instance, crdFinalizer)
+			err := r.Update(ctx, instance)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		// Add finalizer for this CR if not exists
+		if !controllerutil.ContainsFinalizer(instance, crdFinalizer) {
+			controllerutil.AddFinalizer(instance, crdFinalizer)
+			err := r.Update(ctx, instance)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	}
+	return ctrl.Result{}, nil
+}
+
+func checkAndUpdateSecrets(r NamespacedDBCredsReconciler, ctx context.Context, instance *dbv1alpha1.NamespacedDBCreds, log logr.Logger) (ctrl.Result, error) {
+	db, err := sql.Open("mysql", getDataSourceName(instance))
+	if err != nil {
+		log.Error(err, "Unable to connect to database of "+instance.Name)
+		return ctrl.Result{}, err
+	}
+	defer db.Close()
+
+	// Fetch the Namespaces
+	namespaces := &corev1.NamespaceList{}
+	err = r.List(ctx, namespaces)
+	if err != nil {
+		log.Error(err, "Unable to fetch Namespaces")
+		return ctrl.Result{}, err
+	} else {
+		// Create db secret for each Namespace
+		for _, namespace := range namespaces.Items {
+			existingSecret := &corev1.Secret{}
+
+			err = r.Get(ctx, client.ObjectKey{Name: instance.Name, Namespace: namespace.Name}, existingSecret)
+
+			if err != nil && errors.IsNotFound(err) {
+				dbPassword := generateNewPassword(16)
+				if !createDatabase(ctx, db, namespace.Name, dbPassword) {
+					return ctrl.Result{}, err
+				}
+
+				secret := getSecret(instance, &namespace, nil, dbPassword)
+				err = r.Create(ctx, &secret)
+				if err != nil {
+					log.Error(err, "Unable to create Secret in Namespace: "+namespace.Name)
+					return ctrl.Result{}, err
+				} else {
+					// Should set controller & owner references but our usecase is to create secret in all namespaces
+					// If we set references, we will get an error saying `cross-namespace references are disallowed`.
+					log.Info("Secret created in Namespace: " + namespace.Name)
+				}
+			} else if err != nil {
+				log.Error(err, "Unable to fetch Namespace secret: "+namespace.Name)
+				return ctrl.Result{}, err
+			} else {
+				dbPort, _ := strconv.Atoi(string(existingSecret.Data["port"]))
+				if (instance.Spec.DBHost != string(existingSecret.Data["host"])) || (instance.Spec.DBPort != dbPort) {
+					secret := getSecret(instance, &namespace, existingSecret, "")
+
+					if !createDatabase(ctx, db, namespace.Name, generateNewPassword(16)) {
+						log.Error(err, "Unable to create database for Namespace: "+namespace.Name)
+						return ctrl.Result{}, err
+					}
+
+					err = r.Update(ctx, &secret)
+					if err != nil {
+						log.Error(err, "Unable to update Secret in Namespace: "+namespace.Name)
+						return ctrl.Result{}, err
+					} else {
+						log.Info("Secret updated in Namespace: " + namespace.Name)
+					}
+				} else {
+					if !checkDatabaseAndUser(db, namespace.Name) {
+						log.Error(nil, "Database or User got deleted for Namespace: "+namespace.Name)
+						if !createDatabase(ctx, db, namespace.Name, string(existingSecret.Data["password"])) {
+							log.Error(err, "Unable to create database for Namespace: "+namespace.Name)
+							return ctrl.Result{}, err
+						} else {
+							log.Info("Database & User are created for Namespace: " + namespace.Name)
+						}
+					}
+				}
+			}
+		}
+	}
+	return ctrl.Result{}, nil
+}
+
 func getSecret(instance *dbv1alpha1.NamespacedDBCreds, namespace *corev1.Namespace, existingSecret *corev1.Secret, passsword string) corev1.Secret {
 	metadata := metav1.ObjectMeta{
 		Name:      instance.Name,
@@ -251,43 +255,43 @@ func checkDatabaseAndUser(db *sql.DB, namespace string) bool {
 	return err == nil
 }
 
-func createDatabase(db *sql.DB, nsName string, password string) bool {
-	ctx, cancelfunc := context.WithTimeout(context.Background(), 10*time.Second)
+func createDatabase(ctx context.Context, db *sql.DB, nsName string, password string) bool {
+	log := log.FromContext(ctx)
+	dbTxnCtx, cancelfunc := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancelfunc()
 
-	log := log.FromContext(ctx)
-	tx, err := db.BeginTx(ctx, nil)
+	tx, err := db.BeginTx(dbTxnCtx, nil)
 	if err != nil {
 		log.Error(err, "Unable to begin db transaction")
 		return false
 	}
 
 	// Dropping database and user if exists
-	tx.ExecContext(ctx, fmt.Sprintf("DROP DATABASE `%s`", nsName))
-	tx.ExecContext(ctx, fmt.Sprintf("DROP USER `%s`", nsName))
+	tx.ExecContext(dbTxnCtx, fmt.Sprintf("DROP DATABASE `%s`", nsName))
+	tx.ExecContext(dbTxnCtx, fmt.Sprintf("DROP USER `%s`", nsName))
 
-	_, err = tx.ExecContext(ctx, fmt.Sprintf("CREATE DATABASE `%s`", nsName))
+	_, err = tx.ExecContext(dbTxnCtx, fmt.Sprintf("CREATE DATABASE `%s`", nsName))
 	if err != nil {
 		log.Error(err, "Unable to create database: "+nsName)
 		tx.Rollback()
 		return false
 	}
 
-	_, err = tx.ExecContext(ctx, fmt.Sprintf("CREATE USER `%s`@`%%` IDENTIFIED BY '%s'", nsName, password))
+	_, err = tx.ExecContext(dbTxnCtx, fmt.Sprintf("CREATE USER `%s`@`%%` IDENTIFIED BY '%s'", nsName, password))
 	if err != nil {
 		log.Error(err, "Unable to create user: "+nsName)
 		tx.Rollback()
 		return false
 	}
 
-	_, err = tx.ExecContext(ctx, fmt.Sprintf("GRANT ALL PRIVILEGES ON `%[1]s`.* TO `%[1]s`@`%%`", nsName))
+	_, err = tx.ExecContext(dbTxnCtx, fmt.Sprintf("GRANT ALL PRIVILEGES ON `%[1]s`.* TO `%[1]s`@`%%`", nsName))
 	if err != nil {
 		log.Error(err, "Unable to grant privileges for user to database: "+nsName)
 		tx.Rollback()
 		return false
 	}
 
-	_, err = tx.ExecContext(ctx, "FLUSH PRIVILEGES")
+	_, err = tx.ExecContext(dbTxnCtx, "FLUSH PRIVILEGES")
 	if err != nil {
 		log.Error(err, "Unable to flush privileges")
 		tx.Rollback()
@@ -297,11 +301,10 @@ func createDatabase(db *sql.DB, nsName string, password string) bool {
 	err = tx.Commit()
 	if err != nil {
 		log.Error(err, "Unable to commit db transaction")
-		return false
 	} else {
 		log.Info("Schema & user configured for namespace: " + nsName)
-		return true
 	}
+	return err == nil
 }
 
 func getDataSourceName(instance *dbv1alpha1.NamespacedDBCreds) string {
